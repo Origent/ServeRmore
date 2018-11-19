b2421bf8a55296401337c833c00394b1451643cc
Andrew Conklin <aconklin@origent.com>
2018-11-19 18:10:34 -0500

    more fixes

--- ServeRmore.py
+++ ServeRmore.py
@@ -124,8 +124,8 @@ class srm:
         for package in self.cc.settings["builder"]["r_packages"]:
             cloud_connect.run_ssh("echo \"install.packages('"+package+"', repos='http://cran.us.r-project.org')\">> ~/package_install.R")
         if self.cc.settings["builder"]["custom_r_package_file"]:
-            cloud_connect.run_ssh("echo \"system('aws s3 cp s3://"+ self.settings["builder"]["s3_bucket"] +"/"+ self.settings["builder"]["s3_key"] +"/"+ self.settings["builder"]["custom_r_package_file"] + " ./" + self.settings["builder"]["custom_r_package_file"] + "')\">> ~/package_install.R")
-            cloud_connect.run_ssh("echo \"install.packages('"+self.settings["builder"]["custom_r_package_file"]+"', repos=NULL, type = 'source')\">> ~/package_install.R")
+            cloud_connect.run_ssh("echo \"system('aws s3 cp s3://"+ self.cc.settings["builder"]["s3_bucket"] +"/"+ self.cc.settings["builder"]["s3_key"] +"/"+ self.cc.settings["builder"]["custom_r_package_file"] + " ./" + self.cc.settings["builder"]["custom_r_package_file"] + "')\">> ~/package_install.R")
+            cloud_connect.run_ssh("echo \"install.packages('"+self.cc.settings["builder"]["custom_r_package_file"]+"', repos=NULL, type = 'source')\">> ~/package_install.R")
         cloud_connect.terminate_ssh()
         print("Running R Package Installs")
         cloud_connect.initiate_ssh("ec2-user", self.cc.settings["aws"]["private_key"], self.cc.settings["builder"]["domain_name"])
